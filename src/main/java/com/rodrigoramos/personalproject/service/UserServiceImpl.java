9b8fbb16169eda3ca3d09ac57750830e7b6022ea
RodrigoRP <rodrigo.ramos.pa@gmail.com>
2020-01-05 16:49:11 -0300

    Project struct

--- src/main/java/com/rodrigoramos/personalproject/service/UserServiceImpl.java
+++ src/main/java/com/rodrigoramos/personalproject/service/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.rodrigoramos.personalproject.service;
 
 import com.rodrigoramos.personalproject.service.interfaces.UserService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
 }
